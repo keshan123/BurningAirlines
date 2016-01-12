ffcb738407e94ed21eb6b91038d30c586e671a47
Keshan Peiris <keshan.peiris@gmail.com>
2016-01-13 09:25:38 +1100

    shits working

--- app/controllers/session_controller.rb
+++ app/controllers/session_controller.rb
@@ -4,7 +4,7 @@ class SessionController < ApplicationController
 
  def create
    user = User.find_by :email => params[:email]
-   if user.present? && user.authenticate(params[:password])
+   if user.present? && user.authenticate(params[:password]||params[:password_digest])
        session[:user_id] = user.id
        redirect_to root_path
    else
