bec44efa013f2dec7eb95bb9a1b76ccadc85a361
Robert Murray <robert.b.murray@me.com>
2016-01-13 09:50:40 +1100

    Added JS files for BB

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -12,5 +12,10 @@
 //
 //= require jquery
 //= require jquery_ujs
-//= require turbolinks
+//= require underscore
+//= require backbone
+//= require_tree ./models
+//= require_tree ./collections
+//= require_tree ./views
+//= require_tree ./routers
 //= require_tree .
