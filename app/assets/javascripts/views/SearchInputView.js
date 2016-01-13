98149a777e957fbcd0e6b3bf781d2a879b1dc9b9
SUGIMURAWATARU <Ru@WATARU-no-MacBook-Pro.local>
2016-01-13 16:31:10 +1100

    got search function

--- app/assets/javascripts/views/SearchInputView.js
+++ app/assets/javascripts/views/SearchInputView.js
@@ -7,8 +7,28 @@ app.searchInputView = Backbone.View.extend({
     'click button': 'searchFlights'
   },
 
-  searchFlights: function () { // need to pass destination and origin here
+  searchFlights: function () { 
+    // Get the origin out of the select tag
+    var desiredOrigin = this.$el.find("#originSelect").val();
+    // Get the destination out of the select tag
+    var desiredDestination = this.$el.find("#destinationSelect").val();
 
+    // Store the flights with the selected origin and the selected destination as appropriateFlights
+    var appropriateFlights = app.flights.where({ origin: desiredOrigin, destination: desiredDestination });
+
+    $("#flightLists").html(""); // Empty out the flightLists div so it is only the current search results
+
+    if ( appropriateFlights.length === 0 ) {
+      $("#flightLists").html("No Flights found!");
+      return;
+    }
+
+    _.each( appropriateFlights, function (flight) { // Go through each appropriate flight and store the current one as flight
+      // Create a new flightListView passing in the current flight (so it has some data to show)
+      var flightListView = new app.FlightListView({ model: flight });
+      // Call render on that
+      flightListView.render();
+    });
   },
 
   render: function () {
