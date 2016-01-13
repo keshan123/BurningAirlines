935be059e7a3bf8b2acce14d407254e9643ce122
SUGIMURAWATARU <Ru@WATARU-no-MacBook-Pro.local>
2016-01-14 10:23:31 +1100

    some changes on seating

--- app/assets/javascripts/views/FlightView.js
+++ app/assets/javascripts/views/FlightView.js
@@ -11,9 +11,17 @@ app.FlightView = Backbone.View.extend({
 
     if (app.seats){
       var plane = app.seats.get( planeId );
-      console.log(plane)
+      console.log(plane);
       var planeName = plane.get('name');
-      $('#main').append("<h2>" + planeName + "</h2>");
+      $('#main').append("<h2>Plane ID: " + planeName + "</h2>");
+
+      var planeRows = plane.get('rows');
+      var planeRowsArray = _.range( planeRows+1 );
+      $('#main').append("<h2> Rows: " + planeRowsArray + "</h2>");
+
+      var planeColumns = plane.get('columns');
+      var planeColumnsArray = _.range( 'A', 'planeColumns' );
+      $('#main').append("<h2>Columns: " + planeColumnsArray + "</h2>");
 
     }
   }
@@ -29,13 +37,18 @@ app.SeatView = Backbone.View.extend({
     // var planeName = this.model.get('name');
 
     // $('#main').append("<h2>" + planeName + "</h2>");
-
-
   }
 
 });
 
-
+  // create_table "planes", force: :cascade do |t|
+  //   t.string   "name"
+  //   t.integer  "rows"
+  //   t.string   "columns"
+  //   t.integer  "plane_id"
+  //   t.datetime "created_at", null: false
+  //   t.datetime "updated_at", null: false
+  // end
   // create_table "flights", force: :cascade do |t|
   //   t.string   "flightnumber"
   //   t.string   "origin"
