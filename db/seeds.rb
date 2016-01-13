fc4152c9647969a96d42ad18aefa2eef43178329
SUGIMURAWATARU <Ru@WATARU-no-MacBook-Pro.local>
2016-01-13 11:12:25 +1100

    add seed file

--- db/seeds.rb
+++ db/seeds.rb
@@ -11,3 +11,8 @@ u2 = User.create(:name => 'Squiggles', :email => 'squiggles@email.com',  :passwo
 u3 = User.create(:name => 'Zippy', :email => 'zippy@email.com',  :password => 'chicken', :password_confirmation => 'chicken')
 u4 = User.create(:name => 'Lambert', :email => 'lambert@email.com',  :password => 'chicken', :password_confirmation => 'chicken')
 u5 = User.create(:name => 'Bubbles', :email => 'bubbles@email.com',  :password => 'chicken', :password_confirmation => 'chicken')
+
+p1 =Plane.create(:name => 'Boing', :rows => '100',  :columns => 'F')
+p3 =Plane.create(:name => 'Airbus', :rows => '70',  :columns => 'F')
+p4 =Plane.create(:name => 'Mitusbishi', :rows => '40',  :columns => 'F')
+p5 =Plane.create(:name => 'XXXXX', :rows => '200',  :columns => 'F')
