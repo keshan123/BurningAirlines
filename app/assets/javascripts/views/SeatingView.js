b6de5f3beb03880c7052dd2ad61c791b5643a0fe
Robert Murray <robert.b.murray@me.com>
2016-01-13 11:53:37 +1100

    additional setup

---
+++ app/assets/javascripts/views/SeatingView.js
@@ -0,0 +1,10 @@
+var app = app || {};
+
+app.seatingView = Backbone.View.extend({
+  el: "#seatingPlan",
+
+  render: function () {
+    var seatingViewTemplate = $("#secretInputViewTemplate").html();
+    this.$el.html(seatingViewTemplate);
+  }
+});
