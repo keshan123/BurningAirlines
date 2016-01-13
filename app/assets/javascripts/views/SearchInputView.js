1e82396746f00cb09fa2155e09f5eaee8a81ef8d
Robert Murray <robert.b.murray@me.com>
2016-01-13 13:24:15 +1100

    Added some search functionality

--- app/assets/javascripts/views/SearchInputView.js
+++ app/assets/javascripts/views/SearchInputView.js
@@ -1,5 +1,18 @@
 var app = app || {};
 
 app.searchInputView = Backbone.View.extend({
+  el: "#searchForm",
 
+  events: { 
+    'click button': 'searchFlights'
+  },
+
+  searchFlights: function () { // need to pass destination and origin here
+
+  },
+
+  render: function () {
+    var searchInputViewTemplate = $("#searchInputViewTemplate").html();
+    this.$el.html(searchInputViewTemplate);
+  }
 });
